9cb9288e60138ac47ee28e39d7c9844081e5f715
Adria Ciurana <adri00796@gmail.com>
2023-12-18 21:32:55 +1100

    wip

--- evaluator.py
+++ evaluator.py
@@ -54,8 +54,14 @@ class MockEvaluator(Evaluator):
             c.score = 2 * np.random.rand() - 1  # Simulate cosine similarity
 
 
+class SimilarityEvaluator(Evaluator, ABC):
+    @abstractmethod
+    def get_features(self, text: str | list[str]) -> torch.Tensor:
+        ...
+
+
 @Register("Evaluator")
-class BERTSimilarityEvaluator(Evaluator):
+class BERTSimilarityEvaluator(SimilarityEvaluator):
     def __init__(self, device: str = "cuda:0", max_batch: int = 10) -> None:
         super().__init__()
 
@@ -66,12 +72,16 @@ class BERTSimilarityEvaluator(Evaluator):
         self._target_features = torch.Tensor | None
         self.max_batch = max_batch
 
+    @batch_processing(AGGREGATE_TENSORS)
+    def get_features(self, text: str | list[str]) -> torch.Tensor:
+        with torch.no_grad():
+            return self._similarity_model.encode(
+                text, convert_to_tensor=True, show_progress_bar=False
+            )
+
     def init(self, llm: LLM, target: str) -> None:
         super().init(llm, target)
-        with torch.no_grad():
-            self._target_features = self._similarity_model.encode(
-                target, convert_to_tensor=True
-            )
+        self._target_features = self.get_features(target)
 
     def _similarity(
         self, target: torch.Tensor, solutions: torch.Tensor
@@ -80,12 +90,6 @@ class BERTSimilarityEvaluator(Evaluator):
         # Solutions: N x dim
         return util.pytorch_cos_sim(target, solutions)[0]
         # return (target @ solutions.T)[0]
-
-    @batch_processing(AGGREGATE_TENSORS)
-    def _similarity_model_encode(self, prompts: list[str]) -> torch.Tensor:
-        return self._similarity_model.encode(
-            prompts, convert_to_tensor=True, show_progress_bar=False
-        )
 
     def __call__(self, population: list[Chromosome]) -> None:
         assert self._llm is not None
@@ -125,7 +129,96 @@ class BERTSimilarityEvaluator(Evaluator):
                         invalid_nonscored_population.append(c)
 
                 if len(valid_outputs) > 0:
-                    valid_outputs_features = self._similarity_model_encode(
+                    valid_outputs_features = self.get_features(valid_outputs)
+
+                    valid_scores = self._similarity(
+                        self._target_features, valid_outputs_features
+                    )
+
+                else:
+                    valid_scores: list[torch.Tensor] = []
+
+        for c in invalid_nonscored_population:
+            c.score = -1.0
+
+        for c, score in zip(
+            valid_nonscored_population,
+            valid_scores,
+        ):
+            c.score = float(score)
+
+
+@Register("Evaluator")
+class ObjectiveBasedEvaluator(Evaluator):
+    def __init__(
+        self, similarity_evaluator: SimilarityEvaluator, non_blackbox_llm: LLM, k: int
+    ) -> None:
+        super().__init__()
+        self._similarity_evaluator = similarity_evaluator
+        self._non_blackbox_llm = non_blackbox_llm
+        self._k = k
+
+    def init(self, llm: LLM, target: str) -> None:
+        super().init(llm, target)
+
+        # 1. Compute the LLM options
+        prompt = f"""Given the following objective: 
+        {target}
+        Generate an answer.
+        """
+        outputs = self._non_blackbox_llm.generate_from_prompt(self._k * [prompt])
+
+        # 2. Compute the features
+        self._target_features = self._similarity_evaluator.get_features(outputs)
+
+    def _similarity(
+        self, target: torch.Tensor, solutions: torch.Tensor
+    ) -> torch.Tensor:
+        # Take the best solution w.r.t all the targets
+        # Target: 1 x dim
+        # Solutions: N x dim
+        return torch.max(util.pytorch_cos_sim(target, solutions), dim=0)[0]
+
+    def __call__(self, population: list[Chromosome]) -> None:
+        # Very similar than the BERTSimilarityEvaluator, but we have another _similarity function
+        assert self._llm is not None
+        assert self._target is not None
+        assert self._target_features is not None
+
+        nonscored_population: list[Chromosome] = []
+        for c in population:
+            if c.score is None:
+                nonscored_population.append(c)
+
+        if len(nonscored_population) == 0:
+            return
+
+        # invalid: contains:
+        #   - target inside prompt.
+        #   - output inside prompt.
+        #   - prompt inside output.
+        valid_nonscored_population: list[Chromosome] = []
+        invalid_nonscored_population: list[Chromosome] = []
+        valid_outputs: list[str] = []
+        with DisableLogger():
+            with torch.no_grad():
+                outputs = self._llm(nonscored_population)
+                for output, c in zip(outputs, nonscored_population):
+                    c.output = output
+
+                    if (
+                        self._target not in c.prompt
+                        and c.output not in c.prompt
+                        and c.prompt not in c.output
+                    ) or self._llm.IS_NAIVE:
+                        valid_outputs.append(output)
+                        valid_nonscored_population.append(c)
+
+                    else:
+                        invalid_nonscored_population.append(c)
+
+                if len(valid_outputs) > 0:
+                    valid_outputs_features = self._similarity_evaluator.get_features(
                         valid_outputs
                     )
 
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from evaluator import BERTSimilarityEvaluator
+from evaluator import BERTSimilarityEvaluator, ObjectiveBasedEvaluator
 from generator import KeywordGAGenerator, LLMSimilarSentencesGenerator
 from genetic_algorithm import GeneticAlgorithm
-from llm import Phi2
+from llm import M0
 from population_creator import GeneratorPopulationCreator
 
+m0 = M0()
 ga = GeneticAlgorithm(
-    llm=Phi2(),
+    llm=m0,
     population_creator=GeneratorPopulationCreator(20),
-    generator=KeywordGAGenerator(),
-    evaluator=BERTSimilarityEvaluator(),
+    generator=LLMSimilarSentencesGenerator(),
+    evaluator=ObjectiveBasedEvaluator(
+        similarity_evaluator=BERTSimilarityEvaluator(), non_blackbox_llm=m0, k=100
+    ),
 )
 
 ga(
